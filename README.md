7c5d5e3189bef707eb41bb6114df43607bd40de6
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-06-18 13:19:49 +0530

    First commit with basic setup

---
+++ README.md
@@ -0,0 +1,24 @@
+#### Booking Service for Airline
+
+
+` Starting new service`
+
+ - commands
+ ```
+    -npm init
+    -npm i express
+    -npm i body-parser
+    -npm i http-status-codes
+    -npm i mysql2
+    -npm i sequelize
+    -npm i sequelize-cli
+    -npm i nodemon  -> "start": "npx nodemon src/index.js"  in package.json script
+    -npm i dotenv
+    -npm i morgan
+```
+ step -2 after setup basic express server
+ ```
+    -npx sequelize init
+    -npx sequelize db:create
+    
+```
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index')
+const app = express();
+
+const setUpAndStartServer=()=>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT ,async()=>{
+        console.log(`Server started on port : ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+    })
+}
+setUpAndStartServer();
