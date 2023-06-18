5ff4dee04edf70b0db68abc5f703835f43dec8ba
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-06-18 18:19:30 +0530

    Added migration to add new column to booking table

--- README.md
+++ README.md
@@ -23,5 +23,6 @@
     npx sequelize init
     npx sequelize db:create
     npx sequelize model:generate --name Booking --attributes flightId:integer,userId:integer,status:enum
-    
+    npx sequelize migration:create --name modify_bookings_add_new_fields
+    npx sequelize db:migrate
 ```
--- src/models/booking.js
+++ src/models/booking.js
@@ -27,6 +27,16 @@ module.exports = (sequelize, DataTypes) => {
       allowNull:false,
       values: ['InProcess','Booked', 'Cencelled'],
       defaultValue: "InProcess"
+    },
+    noOfSeats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+    },
+    totalCost: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
     }
   }, {
     sequelize,
