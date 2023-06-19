0bc4b02a7b7d299aaca585975a91a216c84f3f67
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-06-19 13:20:28 +0530

    completed create booking

--- src/servises/booking-service.js
+++ src/servises/booking-service.js
@@ -1,6 +1,39 @@
-const {Booking} = require('../models/index');
+const axios = require('axios');
 
-class BookingRepository{
+const { FLIGHT_SERVICE_PATH } = require('../config/serverConfig')
+const {BookingRepository} = require('../repository/index');
+const {ServiceError} = require('../utils/errors/index');
 
+class BookingService{
+    constructor(){
+        this.bookingRepository = new BookingRepository();
+    }
+    async createBooking(data){
+        try {
+            const flightId=data.flightId;
+            let getFlightRequestURL= `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
+            const response = await axios.get(getFlightRequestURL);
+            const flightData = response.data.data;
+            let priceOfTheFlight = flightData.price;
+            if(data.noOfSeats > flightData.totalSeats) {
+                throw new ServiceError('Something went wrong in the booking process', 'Insufficient seats in the flight');
+            }
+            const totalCost = priceOfTheFlight * data.noOfSeats;
+            const bookingPayload = {...data, totalCost};
+            const booking = await this.bookingRepository.create(bookingPayload);
+            const updateFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${booking.flightId}`;
+            console.log(updateFlightRequestURL);
+            await axios.patch(updateFlightRequestURL, {totalSeats: flightData.totalSeats - booking.noOfSeats});
+            const finalBooking = await this.bookingRepository.update(booking.id, {status: "Booked"});
+            return finalBooking;
+        } catch (error) {
+            console.log(error);
+            if(error.name == 'RepositoryError' || error.name == 'ValidationError') {
+                throw error;
+            }
+            throw new ServiceError();
+        }
+    }
+    // home work make update booking
 }
-module.exports = BookingRepository;
+module.exports = BookingService;
